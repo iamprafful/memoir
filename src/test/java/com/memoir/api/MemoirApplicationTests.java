713a862455b6b1df23c77a5fa8236b916e654928
Prafful Lachhwani <prafful.lachhwani@gmail.com>
2022-01-13 11:06:52 +0530

    Initial commit

---
+++ src/test/java/com/memoir/api/MemoirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memoir.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoirApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
